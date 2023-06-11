006e4343c9b3c7ecf9ae5c0b94bfc7c28d9d0cf5
kod <kod@DESKTOP-O55S61N>
2023-06-11 18:18:43 +0100

    implementando interfaces en infrastructure

---
+++ CleanArchitecture.Data/Persistence/StreamerDbContext.cs
@@ -0,0 +1,77 @@
+﻿using CleanArchitecture.Domain;
+using CleanArchitecture.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public class StreamerDbContext : DbContext
+    {
+        public StreamerDbContext(DbContextOptions<StreamerDbContext> options) : base(options)
+        {
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Deleted:
+                        break;
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "system";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "system";
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-O55S61N\SQLEXPRESS; Initial Catalog=Streamer;Integrated Security=True;TrustServerCertificate=True")
+        //    .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+        //    .EnableSensitiveDataLogging();
+        //}
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>()
+                .HasMany(m => m.Videos)
+                .WithOne(m => m.Streamer)
+                .HasForeignKey(m => m.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+
+            modelBuilder.Entity<Video>()
+                .HasMany(p => p.Actores)
+                .WithMany(t => t.Videos)
+                .UsingEntity<VideoActor>(
+                    pt => pt.HasKey(e => new { e.ActorId, e.VideoId })
+                );
+
+
+        }
+
+
+        public DbSet<Streamer>? Streamers { get; set; }
+
+        public DbSet<Video>? Videos { get; set; }
+
+        public DbSet<Actor>? Actores { get; set; }
+
+        public DbSet<Director>? Directores { get; set; }
+
+    }
+}
---
+++ CleanArchitecture.Data/Persistence/StreamerDbContextSeed.cs
@@ -0,0 +1,29 @@
+﻿
+
+using CleanArchitecture.Domain;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public class StreamerDbContextSeed
+    {
+        public static async Task SeedAsync(StreamerDbContext context, ILogger<StreamerDbContextSeed> logger) 
+        {
+            if (!context.Streamers!.Any()) {
+                context.Streamers!.AddRange(GetPreconfiguredStreamer());
+                await context.SaveChangesAsync();
+                logger.LogInformation($"Estamos insertando nuevos {context}", typeof(StreamerDbContext).Name);
+            }
+        }
+
+        public static IEnumerable<Streamer> GetPreconfiguredStreamer() 
+        {
+            return new List<Streamer>
+            {
+                new Streamer { CreatedBy = "ese1", Nombre = "maxi", Url="https://test.com" },
+                new Streamer { CreatedBy = "ese2", Nombre = "sali", Url="https://sali.com" }
+            };
+        }
+
+    }
+}
