4b0f9f60ffe5e581e4d69f7b489011fa94abf898
kod <kod@DESKTOP-O55S61N>
2023-06-11 18:43:17 +0100

    uso de repositorio

---
+++ CleanArchitecture.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,76 @@
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Domain.Common;
+using CleanArchitecture.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Repositories
+{
+
+    public class RepositoryBase<T> : IAsyncRepository<T> where T : BaseDomainModel
+    {
+
+        protected readonly StreamerDbContext _context;
+
+        public RepositoryBase(StreamerDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderby = null, string includeString = null, bool disableTracking = true)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (disableTracking)
+                query = query.AsNoTracking();
+
+            if (!string.IsNullOrWhiteSpace(includeString))
+                query= query.Include(includeString);
+
+            if (orderby != null)
+                return await orderby(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+
+        public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderby = null, List<Expression<Func<T, object>>> includes = null, bool disableTracking = true)
+        {
+            return null;
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
