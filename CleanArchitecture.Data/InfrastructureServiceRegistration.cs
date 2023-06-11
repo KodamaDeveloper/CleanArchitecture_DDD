1f259c47bf6313becd6ab03e7144d60728b1f413
kod <kod@DESKTOP-O55S61N>
2023-06-11 20:07:19 +0100

    e

--- CleanArchitecture.Data/InfrastructureServiceRegistration.cs
+++ CleanArchitecture.Data/InfrastructureServiceRegistration.cs
@@ -1,5 +1,8 @@
-﻿using CleanArchitecture.Application.Contracts.Persistence;
+﻿using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Models;
 using CleanArchitecture.Domain;
+using CleanArchitecture.Infrastructure.Email;
 using CleanArchitecture.Infrastructure.Persistence;
 using CleanArchitecture.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +23,10 @@ namespace CleanArchitecture.Infrastructure
             services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
             services.AddScoped<IVideoRepository, VideoRepository>();
             services.AddScoped<IStreamerRepository, StreamerRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+            return services;
         }
     }
 }
